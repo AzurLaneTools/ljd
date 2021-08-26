99aa3e107acff2b99aa520f001e49447013c480a
AutumnSun1996 <q19960105@163.com>
2021-08-27 01:43:50 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ljd',
-    version='0.1.2',
+    version='0.1.3',
     description='luajit decompiler',
     author='AutumnSun',
     author_email='q19960105@163.com',
