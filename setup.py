ac274119d9733d30c4bf02c42299069f9b095d06
qiushiyang <qiushiyang@corp.netease.com>
2021-08-26 21:46:36 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ljd',
-    version='0.1.0',
+    version='0.1.1',
     description='luajit decompiler',
     author='AutumnSun',
     author_email='q19960105@163.com',
